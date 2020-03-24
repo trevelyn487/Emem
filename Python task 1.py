a2b2ce4cd373020f1bf794353cd78242d676053f
Emem <61992570+trevelyn487@users.noreply.github.com>
2020-03-24 23:05:24 +0100

    Add files via upload

---
+++ Python task 1.py
@@ -0,0 +1,4 @@
+radius = int(input("Provide Radius of Circle in cm: "))
+pi = 3.14159265
+Area_of_circle = float(pi * radius**2)
+print("A circle with Radius of "  + str(radius) + "cm " + "will have an Area of: " + str(Area_of_circle) + "cm2")
